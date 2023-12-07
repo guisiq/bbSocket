0dad511ba0e2976446049351ec8874141a7bb500
guilhermehse <lopesgui01@gmail.com>
2023-12-07 18:29:05 -0300

    exemplo de Middleware

--- ApiUnitins/ApiUnitins/Program.cs
+++ ApiUnitins/ApiUnitins/Program.cs
@@ -20,6 +20,27 @@ namespace ApiUnitins
 
             var app = builder.Build();
 
+            // // Defina os IPs permitidos aqui
+            // var allowedIPs = new HashSet<string> { "192.168.1.1", "192.168.1.2" };
+            // var allowedHosts = new HashSet<string> { "example1.com", "example2.com" };
+
+            // app.Use(async (context, next) =>
+            // {   
+            //     var host = context.Request.Host.Host;
+            //     var remoteIP = context.Connection.RemoteIpAddress?.ToString();
+            //     if (allowedIPs.Contains(remoteIP)|| allowedHosts.Contains(host))
+            //     {
+            //         Console.WriteLine($"Acesso permitido para o IP: {remoteIP}");
+            //         await next.Invoke();
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine($"Acesso negado para o IP: {remoteIP}");
+            //         context.Response.StatusCode = StatusCodes.Status403Forbidden;
+            //         return;
+            //     }
+            // });
+
             // SocketServer e instanciado aqui 
             var socketServer = app.Services.GetRequiredService<SocketServer>();
             // Configure the HTTP request pipeline.
